f40a07b2158df92199e741d51929ccbf31ed6514
wangytangy <yue.bruce.wang@gmail.com>
2017-03-02 10:41:13 -0500

    Finish Add and Search methods

--- trie.js
+++ trie.js
@@ -58,13 +58,13 @@ class Trie {
   add(word, node = this.root) {
     let charIdx = 1
     let currentChar = word.slice(0, charIdx);
-    // console.log(currentNode);
 
     //search for leaf node to insert characters
-    while (node.val === currentChar && charIdx <= word.length) {
+    while (node.children[currentChar] && charIdx <= word.length) {
       node = node.children[currentChar];
       currentChar = word.slice(0, charIdx += 1);
     }
+
     //add new node to current node's children with remaining characters
     //as key
     while (charIdx <= word.length) {
@@ -73,11 +73,38 @@ class Trie {
       currentChar = word.slice(0, charIdx += 1);
     }
   }
+
+  search(word, node = this.root) {
+    let charIdx = 1;
+    let currentChar = word.slice(0, charIdx);
+    while (node.children[currentChar] && charIdx <= word.length) {
+      node = node.children[currentChar];
+      currentChar = word.slice(0, charIdx += 1);
+    }
+
+    //returns null if word doesn't exist
+    //returns node, if val === word
+    if (node.val === word) {
+      return node;
+    } else {
+      return null;
+    }
+  }
+
+  getChildren(prefix, node = this.root) {
+    //returns an array of a children nodes' values
+    let charIdx = 1;
+    let currentChar = prefix.slice(0, charIdx);
+  }
+
 }
 
 let trie = new Trie();
 
-trie.add("car");
+dict.forEach((word) => {
+  trie.add(word);
+});
+trie.add("cardinal");
+
 // console.log(JSON.stringify(trie, null, 4));
-trie.add("cat");
-console.log(JSON.stringify(trie, null, 4));
+console.log(trie.search("cardin"));
