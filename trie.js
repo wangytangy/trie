924c826ca1a5aba959ac2fa3d7f43e248f374d58
wangytangy <yue.bruce.wang@gmail.com>
2017-03-02 00:57:11 -0500

    Attempt Add method

--- trie.js
+++ trie.js
@@ -5,7 +5,7 @@
 
 // INSERTION
 
-const dict = {
+const dict = [
   "car",
   "card",
   "cards",
@@ -15,14 +15,38 @@ const dict = {
   "tr",
   "tri",
   "trie",
-  "tried"
+  "tried",
+  "tween"
+];
+
+/**
+
+this.root = {
+  "c": {
+    "ca": {
+      "car": {
+        "card": {
+          "cardi": {
+            "cardin": {
+              ...
+            }
+          }
+        }
+      },
+      "cat": {
+
+      }
+    }
+  }
 }
 
+
+**/
+
 class Node {
-  constructor(key, value, children) {
-    this.key = key;
-    this.value = undefined;
-    this.children = [];
+  constructor(val = null, children = {}) {
+    this.val = val;
+    this.children = children;
   }
 }
 
@@ -31,9 +55,29 @@ class Trie {
     this.root = new Node();
   }
 
-  add(word) {
+  add(word, node = this.root) {
+    let charIdx = 1
+    let currentChar = word.slice(0, charIdx);
+    // console.log(currentNode);
 
+    //search for leaf node to insert characters
+    while (node.val === currentChar && charIdx <= word.length) {
+      node = node.children[currentChar];
+      currentChar = word.slice(0, charIdx += 1);
+    }
+    //add new node to current node's children with remaining characters
+    //as key
+    while (charIdx <= word.length) {
+      node.children[currentChar] = new Node(currentChar);
+      node = node.children[currentChar];
+      currentChar = word.slice(0, charIdx += 1);
+    }
   }
-
-
 }
+
+let trie = new Trie();
+
+trie.add("car");
+// console.log(JSON.stringify(trie, null, 4));
+trie.add("cat");
+console.log(JSON.stringify(trie, null, 4));
